803353c8c8fc1ce6ca4257558bed6ed49d7c4be8
Om <ombharti888@gmail.com>
2026-02-21 02:02:58 +0530

    feat: add Target model with CRUD operations and aggregation capabilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from '@mui/material/styles';
-import theme from "@/app/_libs/theme";
+import theme from "@/app/_lib/theme";
 
 import "./globals.css";
 
